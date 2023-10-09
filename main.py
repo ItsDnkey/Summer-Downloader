c3b0a585a0d953a09d6704e5fcda3ba7c7956284
ItsDnkey <147354292+ItsDnkey@users.noreply.github.com>
2023-10-09 02:14:08 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,24 @@
-## Soon 
-## Made By @ItsDnkey
+from pytube import YouTube
+
+def Download(link):
+    youtubeObject = YouTube(link)
+    youtubeObject = youtubeObject.streams.get_highest_resolution()
+    try:
+        youtubeObject.download()
+    except:
+        print("An error has occurred")
+    print("Download is completed successfully")
+
+print("""   
+   _____ _    _ __  __ __  __ ______ _____  
+  / ____| |  | |  \/  |  \/  |  ____|  __ \ 
+ | (___ | |  | | \  / | \  / | |__  | |__) |
+  \___ \| |  | | |\/| | |\/| |  __| |  _  / 
+  ____) | |__| | |  | | |  | | |____| | \ \ 
+ |_____/ \____/|_|  |_|_|  |_|______|_|  \_\
+ 
+ By @ItsDnkey on github !                                           
+                                            
+                                            """)
+link = input("Enter the YouTube video URL: ")
+Download(link)
